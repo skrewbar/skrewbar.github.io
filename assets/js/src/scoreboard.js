d38ab51e55abb99668d4ff7c401af16e55a0a16a
skuru <skurua1244@gmail.com>
2024-11-09 13:48:26 +0900

    scoreboard 약간 수정

--- assets/js/src/scoreboard.js
+++ assets/js/src/scoreboard.js
@@ -1,7 +1,6 @@
 const tdList = document.querySelectorAll("td");
-const noSubitEx = /0 \/ --/;
 const WAEx = /[1-9]+ \/ --/;
-const ACEx = /[1-9]+ \/ [0-9]+/;
+const ACEx = /[0-9]+ \/ [0-9]+/;
 
 for (const td of tdList) {
   if (td === td.parentElement.lastElementChild) continue;
